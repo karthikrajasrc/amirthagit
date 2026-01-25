fa5392835c015cac74831a453135b465072ae9cf
karthikrajasrc <karthikrajasrc@gmail.com>
2026-01-25 16:30:42 +0530

    basic of javascript DOM

---
+++ main.js
@@ -0,0 +1,77 @@
+const h1 = document.createElement("h1");
+h1.textContent = "Table of the contents";
+document.body.append(h1);
+
+let olist = ["1. Best Full Stack Development Project Ideas in 2025", "2. Conclusion", "3. Frequently Asked Questions"];
+
+olist.forEach(ols => {
+    const ol = document.createElement("ol");
+    ol.textContent = ols;
+    document.body.append(ol);
+})
+
+
+let ulist = ["E-Commerce Websit", "Online Learning App", "Social Media Platform", "Task Management Application", "Portfolio Website", "Gaming Application", "Quiz App", "Food Management Application"];
+
+ulist.forEach(uls => {
+    const ul = document.createElement("ul");
+    ul.textContent = uls;
+    document.body.appendChild(ul);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let h1 = document.createElement("h1");
+h1.textContent = "Table of contents";
+document.body.append(h1); 
+
+
+
+let olist = ["1. Best Full Stack Development Project Ideas in 2025", "2. Conclusion", "3. Frequently Asked Questions"];
+
+olist.forEach(ols => {
+    const ol = document.createElement("ol");
+    ol.textContent = ols;
+    document.body.append(ol);
+});
+
+
+let ulist = ["E-Commerce Websit", "Online Learning App", "Social Media Platform", "Task Management Application", "Portfolio Website", "Gaming Application", "Quiz App", "Food Management Application"];
+
+ulist.forEach(uls => {
+    const ul = document.createElement("ul");
+    ul.textContent = uls;
+    document.body.append(ul);
+    ul.setAttribute("style", "color : red;");
+}); */
+
+ 
+
+
+
+
