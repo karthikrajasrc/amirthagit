a79245bf6fb880ce28683bcec780f2a08732bc84
karthikrajasrc <karthikrajasrc@gmail.com>
2026-01-29 15:12:50 +0530

    promise and timeout func

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-const h1 = document.createElement("h1");
-h1.textContent = "Table of the contents";
-document.body.append(h1);
-
-let olist = ["1. Best Full Stack Development Project Ideas in 2025", "2. Conclusion", "3. Frequently Asked Questions"];
-
-olist.forEach(ols => {
-    const ol = document.createElement("ol");
-    ol.textContent = ols;
-    document.body.append(ol);
+const promise = new Promise((resolve, reject) => {
+    setTimeout(
+        () => {
+            reject();
+        }, 5000);
 })
 
 
-let ulist = ["E-Commerce Websit", "Online Learning App", "Social Media Platform", "Task Management Application", "Portfolio Website", "Gaming Application", "Quiz App", "Food Management Application"];
-
-ulist.forEach(uls => {
-    const ul = document.createElement("ul");
-    ul.textContent = uls;
-    document.body.appendChild(ul);
-})
+console.log(promise);
 
 
-
-
-
-
-
-
-
-
-
+promise
+    .then(() => {
+        console.log("operation Success");
+        console.log(promise);
+    })
+    .catch(
+        () => {
+            console.log("Operation Failed");
+            console.log(promise);
+        }
+    )
 
 
 
